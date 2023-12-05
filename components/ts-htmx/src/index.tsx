c2846a922e9d5d4db68c972d30b4b2d1d2f00d48
Ismael Taboada Rodero <ismael.jtr@gmail.com>
2023-12-05 21:16:00 +0100

    feat: add events and error handling
    Added events on write request (post, put, delete) to trigger usersChanged
    event in client This event will reload the users table.
    
    Added error handling on create/update users forms. Now, if an error
    occurs this will be shown in the error div in the formularies.

--- components/ts-htmx/src/index.tsx
+++ components/ts-htmx/src/index.tsx
@@ -74,67 +74,45 @@ const app = new Elysia()
     .get("/users", ({ html }) =>
       html(<UserPage />)
     )
-    .post("/users", ({ html, body, set }) => {
-      const user = createUser(
-        {
-          name: body.name,
-          email: body.email
-        }
-      )
-      set.headers["hx-trigger"] = "htmx:closeModal";
-      return html(<UsersTableRow {...user} />)
+    .post("/users", ({ body, set }) => {
+      createUser({ name: body.name, email: body.email })
+      set.headers["hx-trigger"] = "closeModal, usersChanged";
     }, {
       type: "formdata",
       body: t.Object({
-        name: t.String(),
-        email: t.String(),
-      })
+        name: t.String({
+          minLength: 3,
+        }),
+        email: t.String({
+          format: "email",
+        }),
+      }),
     })
-    .put("/users", ({ html, body, set }) => {
-      const user = updateUser(
-        {
-          id: body.id,
-          name: body.name,
-          email: body.email
-        }
-      )
-      set.headers["hx-trigger"] = "htmx:closeModal";
-      return html(<UsersTableRow {...user} />)
+    .put("/users", ({ body, set }) => {
+      updateUser({ id: body.id, name: body.name, email: body.email })
+      set.headers["hx-trigger"] = "closeModal, usersChanged";
     }, {
       type: "formdata",
       body: t.Object({
         id: t.String(),
-        name: t.String(),
-        email: t.String(),
-      })
+        name: t.String({
+          minLength: 3,
+        }),
+        email: t.String({
+          format: "email",
+        }),
+      }),
     })
-    .patch("/users/:id", ({ html, params, body }) => {
-      const user = getUser(params.id);
-      if (!user) {
-        throw new Error("User not found");
-      }
-      const updUser = updateUser(
-        {
-          id: user.id,
-          name: body.name,
-          email: body.email
-        }
-      )
-      return html(<UsersTableRow {...updUser} />)
-    }, {
-      type: "formdata",
-      body: t.Object({
-        id: t.String(),
-        name: t.String(),
-        email: t.String(),
-      })
-    })
-    .delete("/users/:id", ({ params }) => {
+    .delete("/users/:id", ({ params, set }) => {
       const user = getUser(params.id);
       if (!user) {
         throw new Error("User not found");
       }
       deleteUser(params.id);
+      set.headers["hx-trigger"] = "usersChanged";
+    })
+    .get("/_components/users/table", ({ html }) => {
+      return html(<UsersTable />)
     })
     .get("/_components/users/update/:id", ({ html, params }) => {
       const user = getUser(params.id);
@@ -162,11 +140,13 @@ function BaseHtml({ children }: elements.PropsWithChildren) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>HTML-is-DEAD</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
       <link rel="stylesheet" href="/static/css/style.css">
       <script src="https://unpkg.com/htmx.org@1.9.9"></script>
+      <script defer src="https://unpkg.com/htmx.org/dist/ext/debug.js"></script>
+      <script defer src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
     </head>
 
     ${children}  
@@ -177,7 +157,7 @@ function BaseHtml({ children }: elements.PropsWithChildren) {
 // TODO: try to send the header hx-push-url to set the section url in the browser
 function Body({ children, authenticated }: elements.PropsWithChildren<{ authenticated?: boolean }>) {
   return (
-    <body class="bg-dark text-light">
+    <body class="bg-dark text-light" hx-ext="debug, response-targets">
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
@@ -214,7 +194,7 @@ function Body({ children, authenticated }: elements.PropsWithChildren<{ authenti
           </div>
         </div>
       </footer>
-    </body>
+    </body >
   )
 }
 
@@ -298,7 +278,7 @@ function UsersTable() {
   const users = listUsers()
   return (
     <>
-      <table id="users-table" class="table table-dark">
+      <table id="users-table" class="table table-dark" hx-get="/_components/users/table" hx-trigger="usersChanged from:body">
         <thead>
           <tr>
             <th class="col">Name&nbsp;<i class="bi bi-people-fill"></i></th>
@@ -314,7 +294,7 @@ function UsersTable() {
 
 function UsersTableBody({ id, users }: { id: string, users: User[] }) {
   return (
-    <tbody id={id} hx-target="closest tr" hx-swap="outerHTML">
+    <tbody id={id}>
       {users.map(user => (
         <UsersTableRow {...user} />
       ))}
@@ -352,6 +332,7 @@ function UsersTableRowActions({ id }: Partial<User>) {
         type="button"
         class="btn btn-danger"
         hx-delete={`/users/${id}`}
+        hx-swap="none"
       >
         Delete&nbsp;<i class="bi bi-trash-fill"></i>
       </button>
@@ -361,7 +342,11 @@ function UsersTableRowActions({ id }: Partial<User>) {
 
 function UserDialogCreate() {
   return (
-    <form hx-post="/users" hx-target="#users-table-body" hx-swap="beforeend" autocomplete="off">
+    <form
+      autocomplete="off"
+      hx-post="/users"
+      hx-target-error="find [data-errors]"
+    >
       <div class="modal-content text-dark">
         <div class="modal-header">
           <h5 class="modal-title" id="user-modal-label">Create User</h5>
@@ -370,16 +355,19 @@ function UserDialogCreate() {
         <div class="modal-body" id="user-modal-body">
           <div class="container">
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-12">
                 <label for="input-name" class="form-label">Name</label>
                 <input type="text" class="form-control" id="input-name" name="name" placeholder="Name" />
               </div>
             </div>
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-12">
                 <label for="input-email" class="form-label">Email</label>
                 <input type="text" class="form-control" id="input-email" name="email" placeholder="Email" />
               </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 text-danger-emphasis" data-errors></div>
             </div>
           </div>
         </div>
@@ -394,7 +382,11 @@ function UserDialogCreate() {
 
 function UserDialogUpdate({ ...user }: User) {
   return (
-    <form hx-put="/users" hx-target={"#users-table-row-" + user.id} hx-swap="outerHTML" autocomplete="off">
+    <form
+      autocomplete="off"
+      hx-put="/users"
+      hx-target-error="find [data-errors]"
+    >
       <input type="hidden" name="id" value={user.id} />
       <div class="modal-content text-dark">
         <div class="modal-header">
@@ -404,16 +396,19 @@ function UserDialogUpdate({ ...user }: User) {
         <div class="modal-body" id="user-modal-body">
           <div class="container">
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-12">
                 <label for="input-name" class="form-label">Name</label>
                 <input type="text" class="form-control" id="input-name" name="name" placeholder="Name" value={user.name} />
               </div>
             </div>
             <div class="row">
-              <div class="col-md-4">
+              <div class="col-md-12">
                 <label for="input-email" class="form-label">Email</label>
                 <input type="text" class="form-control" id="input-email" name="email" placeholder="Email" value={user.email} />
               </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 text-danger-emphasis" data-errors></div>
             </div>
           </div>
         </div>
@@ -434,12 +429,12 @@ function UserModalContainer() {
         <script>
           {
             `
-          document.body.addEventListener("htmx:closeModal", function(event) {
-            var modalElement = document.querySelector("#user-modal"); // replace with your modal id
-            var bootstrapModal = bootstrap.Modal.getInstance(modalElement);
-            bootstrapModal.hide();
-          });
-          `
+            document.body.addEventListener("closeModal", function(event) {
+              var modalElement = document.querySelector("#user-modal"); // replace with your modal id
+              var bootstrapModal = bootstrap.Modal.getInstance(modalElement);
+              bootstrapModal.hide();
+            });
+            `
           }
         </script>
       </div>
